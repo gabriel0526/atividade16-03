6a19fc15a98263499646c9cb2a6a39362a311119
Gabriel Phellipi <gabrielphellipi13@gmail.com>
2026-03-17 00:35:41 +0000

    teste

---
+++ atividade.py
@@ -0,0 +1,32 @@
+tarefas = []
+
+while True: 
+    print("\n=== TASK MANAGER v1.0 ===")
+    print("1 - criar tarefa")
+    print("2 - listar tarefas")
+    print("3 - sair")
+    
+    opcao = input("\nescolha uma opção: ")
+
+    if opcao not in  ["1", "2", "3"]:
+        print("x opção inválida! Digite 1, 2 ou 3")
+
+    if opcao == "1":
+        nome = input("nome da tarefa: ")
+        if nome.strip():
+            tarefas.append(nome)
+            print(f"Tarefa {nome} criada com sucesso!")
+        else:
+            print("x nome da tarefa não pode ser vazio")
+
+    elif opcao == "2":
+        if len(tarefas) == 0:
+            print("nenhuma tarefa registrada ainda")
+        else:
+            print("\nSuas tarefas")
+            for i, tarefa in enumerate(tarefas, 1):
+                print(f"{i}, {tarefas}")
+
+    elif opcao == "3":
+        print("ate logo")
+        break
